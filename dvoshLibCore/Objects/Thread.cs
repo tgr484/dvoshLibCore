d8c61b88eb697583d5851e9a7c9321714c5a6b7e
shar.ven <shar.ven@ya.ru>
2018-09-27 11:12:01 +0500

    Бьютифайер для тредов

--- dvoshLibCore/Objects/Thread.cs
+++ dvoshLibCore/Objects/Thread.cs
@@ -20,8 +20,25 @@ namespace dvoshLibCore.Objects
         internal Thread(ThreadDto dto)
         {
             _dto = dto;
-            this.Posts = _dto.threads.First().posts.Where(x => x.comment != string.Empty).Select(x => new Post(x)).ToList();
-            
+            this.Posts = BeautifyPosts(_dto.threads.First().posts.Where(x => x.comment != string.Empty).Select(x => new Post(x)).ToList());
+        }
+
+        private List<Post> BeautifyPosts(List<Post> posts)
+        {
+            for(int i = 0; i < posts.Count; ++i)
+            {
+                var matches = Regex.Matches(posts[i].Text, @">>\d*");
+                var replaceStrings =  matches.Select(m => m.Value);
+                foreach(var str in replaceStrings)
+                {
+                    var post = posts.FirstOrDefault(p => p.Id == int.Parse(str.Substring(2)));
+                    if(post != null)
+                    {
+                        posts[i].Text = posts[i].Text.Replace(str, post.Number.ToString());
+                    }
+                }
+            }
+            return posts;
         }
     }
 
@@ -32,7 +49,7 @@ namespace dvoshLibCore.Objects
     {
         private PostInThread _dto;
 
-        public string Text { get; }
+        public string Text { get; set; }
         public string Date { get => _dto.date; }
         public bool IsOp { get => _dto.op == 1 ? true : false; }
         public long Id { get => _dto.num; }
@@ -50,5 +67,10 @@ namespace dvoshLibCore.Objects
             text = Regex.Replace(text, @"&\S*;", string.Empty);
             return Regex.Replace(text, "<[^>]+>", string.Empty);
         }
+
+        //internal void SetText(string text)
+        //{
+        //    this.Text = text;
+        //}
     }
 }
